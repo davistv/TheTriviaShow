86b7533793ff295df53353b5ab6b00758a62b27f
Troy Davis <troy@lamproslabs.com>
2019-06-30 14:53:00 -0400

    Implemented tie breaker questions, no correct answer on last question now triggers the end game check, parsing questions from a csv file, some console improvements to help the host do their job, and a few other improvements to the order of events during game play.

--- js/trivia-show.js
+++ js/trivia-show.js
@@ -1,9 +1,36 @@
-var questions = [];
-questions.push(["What is the standard tie-in knot used by most rock climbers? <br>\n <br>\n A. Square Knot <br>\n B. Double Figure 8 <br>\n C. Bowline <br>\n D. Clove Hitch", "Double Figure 8"]);
-questions.push(['True or False: If a fire ring does not exist, your best option is to make one using rocks in the area.', "False (leave no trace!)"]);
-questions.push(['True or False: Food, cookware and fuel should be stored in the same area of your backpack.', 'False (the fuel can leak)']);
-questions.push(['In what season do most cases of Hypothermia happen?', 'Spring']);
-questions.push(["When cycling on the road, you should: <br>\n<br>\n A. Ride against traffic <br>\n B. Ride with traffic <br>\n C. Ride on the sidewalk <br>\n D. Ride in the middle of the road", 'B. Ride with traffic']);
+var questions;
+var shuffledQuestions;
+var audioStarted = 0;
+var gameStarted = 0;
+var numQuestions = 10;
+var questionsAsked = 0;
+var quickShowPause = 100;
+var mediumShowPause = 500;
+var answerPause = 3000;
+var endGamePause = 12000;
+var contestantKeys = ['a', 'w', 's', 'd', 'f'];
+var contestantQueue = [];
+var showingQuestion  = 0;
+var questionsAskedThisGame = [];
+var thisQuestion;
+
+function loadQuestions() {
+	$.ajax({
+		type: "GET",
+		url: "questions.csv",
+		dataType: "text",
+		success: function(response)  {
+			questions = $.csv.toArrays(response);
+			questions.shift(); // drop header line
+		}
+	}).done(function( data ) {
+		if ( console && console.log ) {
+			// console.log( "Sample of data:", questions.slice( 0, 100 ) );
+		}
+		shuffledQuestions = shuffle(questions);
+		// console.log(questions);
+	});
+}
 
 function key2color(key){
 	switch (key) {
@@ -36,21 +63,6 @@ function shuffle(a) {
 	return a;
 }
 
-var shuffledQuestions = shuffle(questions);
-
-var audioStarted = 0;
-var gameStarted = 0;
-var numQuestions = 5;
-var questionsAsked = 0;
-var answerPause = 3000;
-var endGamePause = 12000;
-var contestantKeys = ['a', 'w', 's', 'd', 'f'];
-var contestantQueue = [];
-var contestantsTurn = 0;
-var showingQuestion  = 0;
-var questionsAskedThisGame = [];
-var thisQuestion;
-
 function randomIntFromInterval(min,max) {
 	return Math.floor(Math.random()*(max-min+1)+min);
 }
@@ -58,16 +70,12 @@ function randomIntFromInterval(min,max) {
 function startup() {
 	console.log("Get ready for Scout Trivia!");
 	gameStarted = 1;
-	$('.start').hide(500);
+	$('.start').hide(mediumShowPause);
 	startGame();
 }
 
 function startGame() {
-	thisQuestion = shuffledQuestions.shift();
-	$('.question').html(thisQuestion[0]);
-	console.log("Q: " + thisQuestion[0]);
-	console.log("A: " + thisQuestion[1]);
-	questionsAsked++;
+	showQuestion();
 }
 
 function initAudio() {
@@ -99,17 +107,20 @@ function randSnd() {
 }
 
 function showQuestion() {
+	$('.question').show(quickShowPause);
 	showingQuestion = 1;
+	contestantQueue = [];
+	$('.player-order').hide(mediumShowPause);
 	thisQuestion = shuffledQuestions.shift();
-	$('.question').html(thisQuestion[0]);
+	var thisHtmlQuestion = thisQuestion[0].replace(/(?:\r\n|\r|\n)/g, '<br>');
+	$('.question').html(thisHtmlQuestion);
 	questionsAsked++;
-	console.log("Question: " + thisQuestion[0]);
-	console.log("Answer: " + thisQuestion[1])
+	console.warn("Question: " + thisQuestion[0]);
+	console.error("Answer: " + thisQuestion[1])
 }
 
 function showAnswer() {
 	showingAnswer = 1;
-	console.log("show answer");
 }
 
 function findWinner() {
@@ -129,16 +140,51 @@ function findWinner() {
 		score_values.push(scores[key]);
 	}
 	
-	var winners = Math.max.apply(null, score_values);
+	var topScore = Math.max.apply(null, score_values);
 	
-	
-	console.log('max scores:');
-	console.log(winners);
+	var numPlayersWithTopScore = 0;
+	var winners = [];
+	for (var key in scores) {
+		if (scores[key] ==  topScore) {
+			numPlayersWithTopScore++;
+			winners.push(key);
+		}
+	}
+	return winners;
+}
+
+function endGameCheck(thisContestant) {
+	var winners = findWinner();
+	if (winners.length == 1) {
+		showingQuestion = 0;
+		console.error(capitalize(key2color(thisContestant)) + " Player Wins!");
+		$('.answer').html("");
+		$('.question').html("Congratulations " + capitalize(key2color(thisContestant)) + " Player!");
+		lowLag.play("sfx/winner.mp3");
+		gameStarted = 0;
+		setTimeout(function(){
+			$('.question').hide(quickShowPause);
+			$('.start').show(mediumShowPause);
+			$('.player-order').hide(quickShowPause);
+			$('.player-order').html("");
+			$('.player-score').html("0");
+			console.clear();
+		}, endGamePause);
+	} else {
+		// tie breaker
+		console.warn("Tie breaker!");
+		showingQuestion = 0;
+		setTimeout(function(){
+			$('.answer').html("");
+			showQuestion();
+		}, answerPause);
+	}
 }
 
 function handleKey(key) {
 	if (!gameStarted) { //  game isn't started yet
 		if (key == 'PageUp') {
+			loadQuestions();
 			startup();
 			lowLag.play("sfx/start.mp3");
 		} else if (key == 'PageDown') {
@@ -154,23 +200,23 @@ function handleKey(key) {
 				contestantQueue.push(key);
 				switch (contestantQueue.length) { // first contestant hit their button after question start
 					case 1:
-						$('.player-order.' + key).html('1st').show();
+						$('.player-order.' + key).html('1st').show(quickShowPause);
 						console.log("1st: Player " + capitalize(key2color(key)));
 						break;
 					case 2:
-						$('.player-order.' + key).html('2nd').show();
+						$('.player-order.' + key).html('2nd').show(quickShowPause);
 						console.log("2nd: Player " + capitalize(key2color(key)));
 						break;
 					case 3:
-						$('.player-order.' + key).html('3rd').show();
+						$('.player-order.' + key).html('3rd').show(quickShowPause);
 						console.log("3rd: Player " + capitalize(key2color(key)));
 						break;
 					case 4:
-						$('.player-order.' + key).html('4th').show();
+						$('.player-order.' + key).html('4th').show(quickShowPause);
 						console.log("4th: Player " + capitalize(key2color(key)));
 						break;
 					case 5:
-						$('.player-order.' + key).html('5th').show();
+						$('.player-order.' + key).html('5th').show(quickShowPause);
 						console.log("5th: Player " + capitalize(key2color(key)));
 						break;
 				}
@@ -180,38 +226,48 @@ function handleKey(key) {
 			thisContestant = contestantQueue.shift();
 			if (key == 'PageUp') {
 				lowLag.play("sfx/correct.mp3");
-				console.log('Correct answer from ' + key2color(thisContestant));
+				console.info('Correct answer from ' + key2color(thisContestant));
 				var oldScore = $('.player-score.' + thisContestant).html();
 				var newScore = parseInt(oldScore) + 1;
 				$('.player-score.' + thisContestant).html(newScore);
-				$('.player-order').hide(500);
+				$('.player-order.' + thisContestant).html("<span class='correct-answer-check'>√</span>");
 				$('.answer').html(thisQuestion[1]);
 				
-				// end game if we have a winner
-				if (questionsAsked == numQuestions) {
-					var winner = findWinner();
-					lowLag.play("sfx/winner.mp3");
-					setTimeout(function(){
-						$('.answer').html("");
-						$('.question').html("Congratulations " + capitalize(key2color(thisContestant)) + " Player!")
-					}, endGamePause);
-				}
-				else {
+				// end game if we have a winner, tie breaker if not
+				if (questionsAsked >= numQuestions) {
+					endGameCheck(thisContestant);
+				} else { // show the next question
+					showingQuestion = 0;
 					setTimeout(function(){
 						$('.answer').html("");
 						showQuestion();
 					}, answerPause);
 				}
-			} else if (key == 'PageDown') {
-				console.log('Wrong anser from ' + key2color(thisContestant));
-				$('.player-order.' + thisContestant).html('X');
+			} else if (key == 'PageDown') { // wrong answer
+				console.error('Wrong anser from ' + key2color(thisContestant));
+				$('.player-order.' + thisContestant).html('<span class="wrong-answer-x">X</span>');
 				lowLag.play("sfx/wrong.mp3");
+				//  Advance to the next question if nobody left in the queue
+				if (contestantQueue.length == 0) {
+					if (questionsAsked >= numQuestions) {
+						endGameCheck(thisContestant);
+					} else {
+						showingQuestion = 0;
+						$('.answer').html(thisQuestion[1]);
+						$('.answer').show(answerPause);
+						setTimeout(function(){
+							$('.answer').hide(quickShowPause);
+							showQuestion();
+						}, answerPause);
+					}
+				}
 			}
 		}
 	}
 }
 
 $(document).ready(function() {
+	loadQuestions();
 	$(document).keydown(function(event) {
 		if (!audioStarted) {
 			initAudio();
